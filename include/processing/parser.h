55c477e5fddf2b0f02b63a366d249bf7d675388a
The-Assembly-Knight <thecodingknight255@gmail.com>
2025-08-06 12:57:00 -0400

    feat: basic parser design

--- include/processing/parser.h
+++ include/processing/parser.h
@@ -1,3 +1,6 @@
 #pragma once
 
+#include "../structs/combo.h"
+#include "../structs/combo_cmd.h"
 
+struct combo_cmd analyze_combo(struct combo *combo);
---
+++ src/processing/parser.c
@@ -0,0 +1,11 @@
+#include "../../include/processing/parser.h"
+
+
+struct combo_cmd analyze_combo(struct combo *combo)
+{
+	struct combo_cmd c_cmd;
+
+	
+
+	return c_cmd;
+}
