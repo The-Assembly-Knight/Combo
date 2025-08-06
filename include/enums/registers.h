98ee30588ff0e21109684460587cacad7edca2cf
The-Assembly-Knight <thecodingknight255@gmail.com>
2025-08-06 12:46:47 -0400

    feat: add combo_cmd struct

---
+++ include/enums/registers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define REGISTERS_AMOUNT 4
+enum reg {NONE, A, B, X, Y};
+
+
---
+++ include/structs/combo_cmd.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include "../enums/registers.h"
+
+struct combo_cmd {
+	enum reg src_reg;
+	enum reg dst_reg;
+
+	unsigned int src_offset;
+	unsigned int dst_offset;
+
+	bool src_macces;
+	bool dst_macces;
+};
