5d5ac21e81965984d9ae7ca3477bc8b55639495c
The-Assembly-Knight <thecodingknight255@gmail.com>
2025-08-07 14:19:43 -0400

    feat: add basic executer struct

---
+++ include/processing/executer.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../structs/combo_cmd.h"
+
+void execute_c_cmd(struct combo_cmd c_cmd);
---
+++ src/processing/executer.c
@@ -0,0 +1,12 @@
+#include "../../include/processing/executer.h"
+
+static int reg_a = 0;
+static int reg_b = 0;
+static int reg_x = 0;
+static int reg_y = 0;
+
+void execute_c_cmd(struct combo_cmd c_cmd)
+{
+	
+}
+
